51d72a85380e30ce9ba2978737473bca38f15479
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-14 17:52:38 -0500

    changed App.js to demo components

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import ClockScreen from './Screens/ClockScreen/ClockScreen';
+import LoadingScreen from './Screens/LoadingScreen/LoadingScreen';
+import { useState } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [loading, setLoading] = useState(true)
+
+  setTimeout(()=>{
+    setLoading(false)
+  },2500)
+
+
+  if(loading){
+    return <LoadingScreen/>
+  }
+  else{
+    return <ClockScreen/>
+  }
+}
