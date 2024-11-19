8bf456a570df11ea185490308caeeebbc4fdca21
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-19 16:24:48 -0500

    added index to map function parameters added alignText parameter to PhantomText changed calculated values to state values to avoid recalculation on re-renders set transparent backgroundColor for non-block chars

--- components/common/PhantomText/PhantomText.jsx
+++ components/common/PhantomText/PhantomText.jsx
@@ -1,31 +1,40 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
 
-const PhantomText = ({text, fontSize = 18, color = "black", displayShadow = true, style = {}}) => {
+const PhantomText = ({text, fontSize = 18, color = "black", displayShadow = true, alignText = "center", style = {}}) => {
 
     const arrayOfChars = text.split("")
 
     return(
         <View
-         style={{flexDirection: 'row', flexWrap: 'wrap', ...style}}>
-        {arrayOfChars.map(char => <PhantomChar color={color} fontSize={fontSize} displayShadow={displayShadow} key={char+Math.random()} char={char}/>)}
+         style={{flexDirection: 'row', flexWrap: 'wrap', ...style, justifyContent: alignText,}}>
+        {arrayOfChars.map((char, i) => <PhantomChar color={color} fontSize={fontSize} displayShadow={displayShadow} key={i} char={char}/>)}
         </View>
     )
 }
 
 const PhantomChar = ({char, color, fontSize, displayShadow}) => {
-    const oppColor = color == "white" ? "black" : "white"
-    const fontFamilies = ['Courier', 'Calibri', 'monospace', 'Arial', 'Helvetica', 'sans-serif', 'serif', 'Roboto']
-    const isBlock = Math.floor(Math.random()*100) > 90
-    const fontIndex = Math.floor(Math.random()*fontFamilies.length)
 
+    // setting the opposite color to use between black & white
+    const oppColor = color == "white" ? "black" : "white"
+    // an array with all the possible font families to use for the text component
+    const fontFamilies = ['Courier', 'Calibri', 'monospace', 'Arial', 'Helvetica', 'sans-serif', 'serif', 'Roboto']
+
+    // defining these properties as state variables to avoid recalculating them on re-render of the parent components
+    const [isBlock, setIsBlock] = useState(Math.floor(Math.random()*100) > 90)
+    const [fontIndex, setFontIndex] = useState(Math.floor(Math.random()*fontFamilies.length))
+
+    // shared styles between all versions of this component
     const baseStyle = {
         fontWeight: 800,
         fontSize: fontSize,
         padding: 1,
         fontFamily: fontFamilies[fontIndex],
         color: isBlock ? oppColor : color,
-        backgroundColor: isBlock? color: "",
+        backgroundColor: isBlock? color : "transparent",
     }
+
+    // styles to be applied when the displayShadow property is set to true
     const shadowStyle = {
         textShadowOffset: isBlock ? {} : {width: -1, height: -1},
         textShadowColor: oppColor,
