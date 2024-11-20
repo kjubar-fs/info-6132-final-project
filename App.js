95dbc949fe566d76238da3ba48377d6595ee6ad6
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-20 13:05:18 -0500

    Upgrade expo version and fix audit vulnerabilities
    Also fixes an error with default exports from newest version

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Animated, useAnimatedValue } from 'react-native';
 
 import { HomeScreen } from './Screens/HomeScreen';
+import { ClockScreen } from "./Screens/ClockScreen/ClockScreen";
 import { LoadingScreen } from './Screens/LoadingScreen/LoadingScreen';
 
 import { AppLoader } from './components/AppLoader';
@@ -25,7 +26,7 @@ export default function App() {
     return (
         <>
             <AppLoader onLoaded={fadeOutLoading} />
-            <HomeScreen />
+            <ClockScreen />
             {loading &&
                 <Animated.View style={{position: "absolute", width: "100%", height: "100%", opacity: fadeAnim}}>
                     <LoadingScreen />
