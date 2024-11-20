26b52c97f67709d26967902aa062fc9881f6e6da
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-20 15:58:23 -0500

    Add initial data mappings for API data

---
+++ utils/dataMaps.js
@@ -0,0 +1,28 @@
+export const elementList = [
+    "phys",
+    "gun",
+    "fire",
+    "ice",
+    "elec",
+    "wind",
+    "psy",
+    "nuke",
+    "bless",
+    "curse",
+];
+
+export const personaAffinities = {
+    "-": "neutral",
+    "wk": "weak",
+    "rs": "resist",
+    "nu": "null",
+    "ab": "absorb",
+    "rp": "repel",
+};
+
+export const partyAffinities = {
+    "-": "neutral",
+    "w": "weak",
+    "s": "resist",
+    "n": "null",
+};
