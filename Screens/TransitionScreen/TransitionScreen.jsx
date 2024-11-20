1338af969a0ccbf45916a9eb7d401d6517ff12ea
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-19 21:26:27 -0500

    TransitionScreen created added jokerWalking to assets

---
+++ Screens/TransitionScreen/TransitionScreen.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { View, Image } from "react-native";
+
+const TransitionScreen = () => {
+
+    const [xPosition, setXPosition] = useState(900)
+
+    useEffect(()=>{
+        const transition = setInterval(()=>{
+            setXPosition(prev => { return prev > 0 ? prev - 5 : prev })
+        },50)
+
+        return () => {
+            clearInterval(transition)
+        }
+    },[])
+
+
+    return(
+        <View
+            style={{
+                backgroundColor: '#cc0000',
+                width: '100%',
+                height: '100%',
+            }}
+        >
+            <Image
+                source={require('../../assets/jokerWalking.gif')}
+                style={{
+                    position: 'absolute',
+                    left: xPosition -800,
+                    width: 1200,
+                    height: '100%',
+                    paddingRight: 300,
+                    backgroundColor: 'black',
+                }}
+            />
+        </View>
+    )
+}
+
+export default TransitionScreen
