c29c622dec1bb5821015c212aab87ebfe85d661c
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-20 11:18:56 -0500

    Add AppLoader component

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import ClockScreen from './Screens/ClockScreen/ClockScreen';
-import LoadingScreen from './Screens/LoadingScreen/LoadingScreen';
 import { useState } from 'react';
 
+import ClockScreen from './Screens/ClockScreen/ClockScreen';
+import LoadingScreen from './Screens/LoadingScreen/LoadingScreen';
+
+import { AppLoader } from './components/AppLoader';
+
 export default function App() {
+    const [loading, setLoading] = useState(true);
 
-  const [loading, setLoading] = useState(true)
+    let content = <ClockScreen />;
+    if (loading) {
+        content = <LoadingScreen />;
+    }
 
-  setTimeout(()=>{
-    setLoading(false)
-  },2500)
-
-
-  if(loading){
-    return <LoadingScreen/>
-  }
-  else{
-    return <ClockScreen/>
-  }
+    return (
+        <>
+            <AppLoader onLoaded={() => setLoading(false)} />
+            {content}
+        </>
+    );
 }
