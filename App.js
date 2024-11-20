b08d2f2bc6cb910dc9e27fa4a501a42e3d3de694
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-20 14:16:38 -0500

    added Transition proof of concept to `App.js`

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
 import ClockScreen from './Screens/ClockScreen/ClockScreen';
 import LoadingScreen from './Screens/LoadingScreen/LoadingScreen';
 import { useState } from 'react';
+import TransitionScreen from './Screens/TransitionScreen/TransitionScreen';
 
 export default function App() {
 
   const [loading, setLoading] = useState(true)
+  const [showTransition, setShowTransition] = useState(true)
 
   setTimeout(()=>{
     setLoading(false)
-  },2500)
+  },3000)
 
+  setTimeout(()=>{
+    setShowTransition(false)
+  },10000)
 
   if(loading){
     return <LoadingScreen/>
+  }
+
+  if(showTransition){
+    return <TransitionScreen duration={8000}/>
   }
   else{
     return <ClockScreen/>
