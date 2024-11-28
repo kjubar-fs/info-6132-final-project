040c6a30c71ea550c974e85d8603b48bbeaad440
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-28 13:43:06 -0500

    login/register logic added to LoginScreen

--- Screens/LoginScreen/LoginScreen.jsx
+++ Screens/LoginScreen/LoginScreen.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { View, Text, ImageBackground, TextInput, StyleSheet, Pressable, Button } from "react-native";
+import { View, Text, ImageBackground, TextInput, Pressable, Button } from "react-native";
+import { areFieldsValid, signIn } from "../../services/auth/firebaseAuth";
 import styles from "./LoginScreenStyles";
 
 
@@ -8,8 +9,44 @@ export const LoginScreen = () => {
     const [isReturningUser, setIsReturningUser] = useState(true)
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [alert, setAlert] = useState("")
 
     const toggleScreen = () => { setIsReturningUser(prev => !prev)}
+
+    const loginAttempt = async() => {
+        setAlert("")
+
+        console.log(email,password)
+
+        const validation = areFieldsValid(email, password)
+        
+        if(!validation.success){
+            setAlert(validation.message)
+            return
+        }
+
+        const loginResult = await signIn(email,password)
+        if(!loginResult.success){
+            setAlert(loginResult.message)
+            return
+        }
+    }
+
+    const registerAttempt = async() => {
+        setAlert("")
+        const validation = areFieldsValid(email, password)
+        
+        if(!validation.success){
+            setAlert(validation.message)
+            return
+        }
+
+        const registerResult = await signUp(email,password)
+        if(!registerResult.success){
+            setAlert(registerResult.message)
+            return
+        }
+    }
     
 
     return(
@@ -26,6 +63,7 @@ export const LoginScreen = () => {
                         style={styles.inputField}
                         value={email}
                         onChange={setEmail}
+                        keyboardType="email"
                         />
                     </View>
                     <View style={styles.inputContainer}>
@@ -40,9 +78,10 @@ export const LoginScreen = () => {
                     <View style={styles.btnContainer}>
                         <Button
                             title={isReturningUser ? "Login" : "Register"}
-                            onPress={()=>{console.log(isReturningUser)}}
+                            onPress={isReturningUser ? loginAttempt : registerAttempt}
                         />
                     </View>
+                    <Text style={styles.alert}>{alert}</Text>
                 </View>
                 <Pressable
                     onPress={toggleScreen}
