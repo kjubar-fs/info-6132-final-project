f6da537d475ea1aa035239f1669c0a4ca43dd5e7
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-27 21:54:54 -0500

    Render party list

--- Screens/PartyScreen/index.jsx
+++ Screens/PartyScreen/index.jsx
@@ -1,7 +1,23 @@
-import { Text } from "react-native";
+import { Text, FlatList, TouchableOpacity } from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
+
+// TODO: replace this with data from API
+import partyData from "../../dummyData/PartyDataRoyal.json";
 
 export function PartyScreen() {
+    const navigation = useNavigation();
+
     return (
-        <Text>Party members go here</Text>
+        <FlatList
+            data={Object.keys(partyData)}
+            renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => navigation.navigate("PartyDetailScreen", { partyMemberName: item })} key={item}>
+                    <Text>{item}</Text>
+                </TouchableOpacity>
+            )}
+            style={{padding: 10}}
+            contentContainerStyle={{gap: 10, paddingBottom: 10}}
+        />
     );
 }
