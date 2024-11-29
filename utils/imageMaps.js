c006e930a7dbdb61c176bd96fe3d9aa23ac88d4b
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-29 12:44:59 -0500

    Add map object for item images
    Image references to local assets in React Native are statically compiled and not resolved at runtime, so there's no way to do a dynamic `require()` call. Because we want at least some image assets stored locally, we have to manually assemble an object that maps properties to `require()` calls, which can then be used to dynamically pick the image.

---
+++ utils/imageMaps.js
@@ -0,0 +1,98 @@
+export const itemIcons = {
+    "Accessory": require("../assets/items/accessory.png"),
+    "skillCard": require("../assets/items/skillCard.png"),
+    "Gun": {
+        "Akechi": {
+            "standard": require("../assets/items/guns/standard/akechi.png"),
+            "rare": require("../assets/items/guns/rare/akechi.png"),
+        },
+        "Ann": {
+            "standard": require("../assets/items/guns/standard/ann.png"),
+            "rare": require("../assets/items/guns/rare/ann.png"),
+        },
+        "Haru": {
+            "standard": require("../assets/items/guns/standard/haru.png"),
+            "rare": require("../assets/items/guns/rare/haru.png"),
+        },
+        "Joker": {
+            "standard": require("../assets/items/guns/standard/joker.png"),
+            "rare": require("../assets/items/guns/rare/joker.png"),
+        },
+        "Kasumi": {
+            "standard": require("../assets/items/guns/standard/kasumi.png"),
+            "rare": require("../assets/items/guns/rare/kasumi.png"),
+        },
+        "Makoto": {
+            "standard": require("../assets/items/guns/standard/makoto.png"),
+            "rare": require("../assets/items/guns/rare/makoto.png"),
+        },
+        "Morgana": {
+            "standard": require("../assets/items/guns/standard/morgana.png"),
+            "rare": require("../assets/items/guns/rare/morgana.png"),
+        },
+        "Ryuji": {
+            "standard": require("../assets/items/guns/standard/ryuji.png"),
+            "rare": require("../assets/items/guns/rare/ryuji.png"),
+        },
+        "Yusuke": {
+            "standard": require("../assets/items/guns/standard/yusuke.png"),
+            "rare": require("../assets/items/guns/rare/yusuke.png"),
+        },
+    },
+    "Protector": {
+        "Men": {
+            "standard": require("../assets/items/protectors/male.png"),
+            "rare": require("../assets/items/protectors/male.png"),
+        },
+        "Morgana": {
+            "standard": require("../assets/items/protectors/morgana.png"),
+            "rare": require("../assets/items/protectors/morgana.png"),
+        },
+        "Unisex": {
+            "standard": require("../assets/items/protectors/unisex.png"),
+            "rare": require("../assets/items/protectors/unisex.png"),
+        },
+        "Women": {
+            "standard": require("../assets/items/protectors/female.png"),
+            "rare": require("../assets/items/protectors/female.png"),
+        },
+    },
+    "Weapon": {
+        "Akechi": {
+            "standard": require("../assets/items/weapons/akechi.png"),
+            "rare": require("../assets/items/weapons/akechi.png"),
+        },
+        "Ann": {
+            "standard": require("../assets/items/weapons/ann.png"),
+            "rare": require("../assets/items/weapons/ann.png"),
+        },
+        "Haru": {
+            "standard": require("../assets/items/weapons/haru.png"),
+            "rare": require("../assets/items/weapons/haru.png"),
+        },
+        "Joker": {
+            "standard": require("../assets/items/weapons/joker.png"),
+            "rare": require("../assets/items/weapons/joker.png"),
+        },
+        "Kasumi": {
+            "standard": require("../assets/items/weapons/kasumi.png"),
+            "rare": require("../assets/items/weapons/kasumi.png"),
+        },
+        "Makoto": {
+            "standard": require("../assets/items/weapons/makoto.png"),
+            "rare": require("../assets/items/weapons/makoto.png"),
+        },
+        "Morgana": {
+            "standard": require("../assets/items/weapons/morgana.png"),
+            "rare": require("../assets/items/weapons/morgana.png"),
+        },
+        "Ryuji": {
+            "standard": require("../assets/items/weapons/ryuji.png"),
+            "rare": require("../assets/items/weapons/ryuji.png"),
+        },
+        "Yusuke": {
+            "standard": require("../assets/items/weapons/yusuke.png"),
+            "rare": require("../assets/items/weapons/yusuke.png"),
+        },
+    },
+};
