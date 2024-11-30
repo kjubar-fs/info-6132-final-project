7e08df7f39ec4edeaaa116e4fd0ab7062b7c5406
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-29 19:49:45 -0500

    added testing temporal code to showcase component

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ import { Animated, useAnimatedValue } from 'react-native';
 import { ClockScreen } from './Screens/ClockScreen/ClockScreen';
 import { LoadingScreen } from './Screens/LoadingScreen/LoadingScreen';
 
+// TODO: REMOVE IMPORTS AFTER TESTING
+import { AnimatedArcana } from './components/AnimatedArcana/AnimatedArcana';
+import { TestChildrenForArcana } from './components/AnimatedArcana/TestChildrenForArcana';
+// END OF TODO
+
 import { AppLoader } from './components/AppLoader';
 
 export default function App() {
@@ -21,6 +26,14 @@ export default function App() {
             setLoading(false);
         });
     };
+
+    // TODO: REMOVE THIS RETURN AFTER TESTING
+    return(
+        <AnimatedArcana delayContent={true} delayBackground={false}>
+            <TestChildrenForArcana/>
+        </AnimatedArcana>
+    )
+    // END OF TODO
 
     return (
         <>
