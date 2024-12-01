e35cbfa745bb45fceddb9b03b124dfdc799f36b9
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-12-01 18:13:11 -0500

    removed TODO from FavouritesScreen

--- Screens/FavouritesScreen/FavouritesScreen.jsx
+++ Screens/FavouritesScreen/FavouritesScreen.jsx
@@ -2,9 +2,6 @@ import { Text, TouchableOpacity, FlatList } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 
-// TODO: replace this with data from API
-import personaData from "../../dummyData/PersonaDataRoyal.json";
-
 import { safeAreaBottomPadding } from "../../utils/constants";
 import { useEffect, useState } from "react";
 import { getFavouritesForUser } from "../../services/database/firebaseDb";
