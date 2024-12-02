f8ec24f4609614c89e589a4623d8757f08e76acf
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-12-02 18:32:21 -0500

    Add basic persona list styling

--- Screens/PersonasScreen/index.jsx
+++ Screens/PersonasScreen/index.jsx
@@ -5,20 +5,27 @@ import { useNavigation } from "@react-navigation/native";
 // TODO: replace this with data from API
 import personaData from "../../dummyData/PersonaDataRoyal.json";
 
+import { BackgroundWrapper } from "../../components/common/BackgroundWrapper";
+import { ListHeader } from "../../components/common/ListHeader";
+
 import { safeAreaBottomPadding } from "../../utils/constants";
 
 export function PersonasScreen() {
     const navigation = useNavigation();
 
     return (
-        <FlatList
-            data={Object.keys(personaData)}
-            renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => navigation.navigate("PersonaDetailScreen", { personaName: item })} key={item}>
-                    <Text>{item}</Text>
-                </TouchableOpacity>
-            )}
-            contentContainerStyle={{gap: 10, padding: 10, paddingBottom: safeAreaBottomPadding}}
-        />
+        <BackgroundWrapper>
+            <ListHeader title="Personas" />
+
+            <FlatList
+                data={Object.keys(personaData)}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate("PersonaDetailScreen", { personaName: item })} key={item}>
+                        <Text style={{color: "white"}}>{item}</Text>
+                    </TouchableOpacity>
+                )}
+                contentContainerStyle={{gap: 10, padding: 10, paddingBottom: safeAreaBottomPadding}}
+            />
+        </BackgroundWrapper>
     );
 }
