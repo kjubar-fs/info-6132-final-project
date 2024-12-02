7646c80bcdeebcf1d42ac1ed874cef75ddb869d2
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-12-02 14:28:33 -0500

    Add override prop

--- components/common/BackgroundWrapper/index.jsx
+++ components/common/BackgroundWrapper/index.jsx
@@ -2,10 +2,10 @@ import { View, Image } from "react-native";
 
 import styles from "./styles";
 
-export function BackgroundWrapper({ children }) {
+export function BackgroundWrapper({ children, imageOverride = undefined }) {
     return (
         <View style={styles.screenWrapper}>
-            <Image source={require("../../../assets/chainsBg.webp")} style={styles.background} />
+            <Image source={imageOverride ? imageOverride : require("../../../assets/chainsBg.webp")} style={styles.background} />
             {children}
         </View>
     );
