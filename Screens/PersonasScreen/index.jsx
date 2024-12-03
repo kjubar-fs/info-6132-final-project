69de34a987111745a00f1d83f0c0f48216147e7d
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-12-02 19:38:03 -0500

    Style persona entries

--- Screens/PersonasScreen/index.jsx
+++ Screens/PersonasScreen/index.jsx
@@ -1,16 +1,17 @@
-import { Text, TouchableOpacity, FlatList } from "react-native";
+import { View, Text, Pressable, FlatList } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 
 import { useApi } from "../../hooks/useApi";
 
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 import { BackgroundWrapper } from "../../components/common/BackgroundWrapper";
 import { ListHeader } from "../../components/common/ListHeader";
 
-import { safeAreaBottomPadding } from "../../utils/constants";
+import styles from "./styles";
 
 export function PersonasScreen({ favourites = undefined }) {
-    const navigation = useNavigation();
     const personaData = useApi().persona;
 
     return (
@@ -18,14 +19,51 @@ export function PersonasScreen({ favourites = undefined }) {
             <ListHeader title={favourites !== undefined ? "Favourites" : "Personas"} />
 
             <FlatList
-                data={favourites !== undefined ? favourites : Object.keys(personaData)}
+                data={favourites !== undefined ? favourites : Object.keys(personaData).sort()}
                 renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => navigation.navigate("PersonaDetailScreen", { personaName: item })} key={item}>
-                        <Text style={{color: "white"}}>{item}</Text>
-                    </TouchableOpacity>
+                    <Persona key={item} name={item} />
                 )}
-                contentContainerStyle={{gap: 10, padding: 10, paddingBottom: safeAreaBottomPadding}}
+                contentContainerStyle={styles.listContainer}
             />
         </BackgroundWrapper>
+    );
+}
+
+function Persona({ name }) {
+    const navigation = useNavigation();
+    const personaData = useApi().persona;
+    const persona = personaData[name];
+    const displayName = name.endsWith("Picaro") ? 
+        name.slice(0, -3) + "." :
+        name;
+    const isRare = persona.rare;
+    const isDLC = persona.dlc;
+
+    return (
+        <Pressable onPress={() => navigation.navigate("PersonaDetailScreen", { personaName: name })}>
+            {({ pressed }) => (
+                <View style={styles.nameContainer}>
+                    <Text style={[
+                        styles.arcana,
+                        pressed ? styles.arcanaPressed : undefined,
+                        isRare ? styles.arcanaRare : undefined,
+                        isDLC ? styles.arcanaDLC : undefined,
+                    ]}>{persona.arcana}</Text>
+                    <Text style={[
+                        styles.name,
+                        pressed ? styles.namePressed : undefined,
+                        isRare ? styles.nameRare : undefined,
+                        isDLC ? styles.nameDLC : undefined,
+                    ]}>{displayName}</Text>
+                    {isRare &&
+                        <Ionicons name="diamond-sharp" size={pressed ? 26 : 24} color="#A3EB6B" />}
+                    {isDLC &&
+                        <Text style={[
+                            styles.tag,
+                            pressed ? styles.tagPressed : undefined,
+                        ]}>DLC</Text>}
+                </View>
+            )}
+        </Pressable>
     );
 }
