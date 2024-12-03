3459e4f9494d871752415ee54673106e0b139c2d
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-12-02 20:47:12 -0500

    Fix content extending beyond screen on Android

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
 import { Platform } from "react-native";
 
 export const safeAreaTopPadding = Platform.OS === "ios" ? 60 : 50;
-export const safeAreaBottomPadding = Platform.OS === "ios" ? 30 : 10;
+export const safeAreaBottomPadding = Platform.OS === "ios" ? 30 : 50;
